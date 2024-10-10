69538ea67c5898dd79847545d4bee9b688842e5c
Terence Zengeya <terencetamuka@gmail.com>
2024-10-10 13:24:20 +0000

    Added the due date filed to the form and my to do list view

--- todo/forms.py
+++ todo/forms.py
@@ -6,7 +6,14 @@ from .models import TodoItem
 class TodoForm(forms.ModelForm):
     class Meta:
         model = TodoItem
-        fields = ['title', 'description']
+        fields = ['title', 'due_date', 'description']
         widgets = {
+            'due_date': forms.widgets.DateTimeInput(attrs={'type': 'datetime-local'}),
             'description': SummernoteWidget(),
         }
+
+    # def clean_due_date(self):
+    #     due_date = self.cleaned_data.get('due_date')
+    #     if due_date and due_date < timezone.now():
+    #         raise forms.ValidationError("The due date cannot be in the past.")
+    #     return due_date
